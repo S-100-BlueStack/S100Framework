dda54d5681489a1bb5c28f897677c262a8a2863c
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-03-07 10:59:12 +0100

    Added: S100Framework.ProductCatalogue

--- nuget_publish.bat
+++ nuget_publish.bat
@@ -3,3 +3,4 @@ dotnet nuget push ".\src\S100Framework.Catalogues\bin\Debug\*.nupkg" --api-key %
 dotnet nuget push ".\src\S100Framework.GML\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
 dotnet nuget push ".\src\S100Framework.Topology\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
 dotnet nuget push ".\src\S100Framework.YAML\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
+dotnet nuget push ".\src\S100Framework.ProductCatalogue\bin\x64\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
