edc7743e645dc9eb498abbc6cd22c9ce5d66c467
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-04-03 16:26:29 +0200

    S100Framework.YAML\bin\x64\*.nupkg

--- nuget_publish.bat
+++ nuget_publish.bat
@@ -3,5 +3,5 @@ dotnet nuget push ".\src\S100Framework.Catalogues\bin\Debug\*.nupkg" --api-key %
 dotnet nuget push ".\src\S100Framework.Catalogues.ProductSpecifications\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
 dotnet nuget push ".\src\S100Framework.GML\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
 dotnet nuget push ".\src\S100Framework.Topology\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
-dotnet nuget push ".\src\S100Framework.YAML\bin\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
+dotnet nuget push ".\src\S100Framework.YAML\bin\x64\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
 dotnet nuget push ".\src\S100Framework.ProductCatalogue\bin\x64\Debug\*.nupkg" --api-key %github-api-key% --source "github" --skip-duplicate
