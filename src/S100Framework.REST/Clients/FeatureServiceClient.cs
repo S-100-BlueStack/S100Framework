d48c1456c38b7d0790131b6c34e30376f76a20ea
Kasper Soelberg Rosenfeldt <mahizzta@gmail.com>
2026-04-27 14:07:49 +0200

    Move related-record query operations to partial client file

--- src/S100Framework.REST/Clients/FeatureServiceClient.cs
+++ src/S100Framework.REST/Clients/FeatureServiceClient.cs
@@ -58,55 +58,6 @@ public sealed partial class FeatureServiceClient : IFeatureServiceClient
         _serviceUri = _options.ServiceUri ?? throw new InvalidOperationException("ServiceUri must be configured.");
     }
 
-    internal Task<EsriRelatedRecordsResponseDto> QueryRelatedRecordsAsync(
-        int layerId,
-        RelatedRecordsQuery query,
-        CancellationToken cancellationToken = default) {
-        ArgumentNullException.ThrowIfNull(query);
-        query.Validate();
-
-        var parameters = new Dictionary<string, string?> {
-            ["f"] = "json",
-            ["objectIds"] = string.Join(",", query.ObjectIds),
-            ["relationshipId"] = query.RelationshipId.ToString(CultureInfo.InvariantCulture),
-            ["outFields"] = query.OutFields is { Count: > 0 }
-                ? string.Join(",", query.OutFields)
-                : "*",
-            ["definitionExpression"] = query.DefinitionExpression,
-            ["returnGeometry"] = query.ReturnGeometry ? "true" : "false",
-            ["returnTrueCurves"] = _options.ReturnTrueCurves ? "true" : "false",
-            ["orderByFields"] = query.OrderBy
-        };
-
-        if (query.OutSrid.HasValue) {
-            parameters["outSR"] = query.OutSrid.Value.ToString(CultureInfo.InvariantCulture);
-        }
-
-        if (query.ReturnZ.HasValue) {
-            parameters["returnZ"] = query.ReturnZ.Value ? "true" : "false";
-        }
-
-        if (query.ReturnM.HasValue) {
-            parameters["returnM"] = query.ReturnM.Value ? "true" : "false";
-        }
-
-        if (query.GeometryPrecision.HasValue) {
-            parameters["geometryPrecision"] = query.GeometryPrecision.Value.ToString(CultureInfo.InvariantCulture);
-        }
-
-        if (query.MaxAllowableOffset.HasValue) {
-            parameters["maxAllowableOffset"] = query.MaxAllowableOffset.Value.ToString(CultureInfo.InvariantCulture);
-        }
-
-        var uri = UriUtility.WithQuery(
-            UriUtility.AppendPath(
-                _serviceUri,
-                $"{layerId.ToString(CultureInfo.InvariantCulture)}/queryRelatedRecords"),
-            parameters);
-
-        return GetAsync<EsriRelatedRecordsResponseDto>(uri, cancellationToken);
-    }
-
     internal Task<EsriAttachmentQueryResponseDto> QueryAttachmentsAsync(
         int layerId,
         AttachmentQuery query,
