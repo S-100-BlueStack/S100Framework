3edf8d1ebe8169b3693bad0dcf97dc025ffa3f1d
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-08-10 19:30:39 +0200

    ... gettting close!

--- src/S100Framework.ArcGIS.Core/GeometryExtensions.cs
+++ src/S100Framework.ArcGIS.Core/GeometryExtensions.cs
@@ -1,6 +1,7 @@
 ﻿//#define SKIN_OF_THE_EARTH_ONLY
 
 using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.UtilityNetwork.Trace;
 using ArcGIS.Core.SystemCore;
 using Microsoft.Extensions.Logging;
 using NetTopologySuite.Geometries;
@@ -114,6 +115,320 @@ namespace ArcGIS.Core.Geometry
         private const string surfaceTopologyFeatures = "'DEPTHAREA','DREDGEDAREA','LANDAREA','UNSURVEYEDAREA','SHORELINECONSTRUCTION'";
         private const string curveTopologyFeatures = "'COASTLINE','DEPTHCONTOUR','SHORELINECONSTRUCTION'";
 
+        public delegate IEnumerable<(long objectid, string UID, Geometry shape)> FeatureQuery(string tablename, string whereClause, string de9im);
+
+        public static (S100FC.Topology.IMatrix matrix, IDictionary<string, string> mapper) BuildTopology(this Geodatabase geodatabase, FeatureQuery features, Action<int, ICollection<(LineString lineString, string message)>, bool>? interceptor = default, ILoggerFactory? loggerFactory = default) {
+            S100FC.Topology.Matrix.Factory = S100FC.Topology.Reloaded.Factory = factory;
+
+            var definitions = geodatabase.GetDefinitions<FeatureClassDefinition>();
+
+
+            var logger = loggerFactory?.CreateLogger<Reloaded>();
+
+            var matrix = S100FC.Topology.Reloaded.CreateMatrix(interceptor, logger);
+
+            S100FC.Topology.ITopologyBuilder? builder = default;
+
+            var mapper = new Dictionary<string, string>();
+
+            //  Skin of the Earth
+            {
+                var polygons = new List<S100FC.Topology.Polygon>();
+
+                string[] de9im = [Matrix.DE9IM_Contains, Matrix.DE9IM_Crosses];
+                for (int j = 0; j < de9im.Length; j++) {
+                    var lookup = polygons.ToLookup(e => e.ObjectId, e => e);
+
+                    foreach (var f in features.Invoke("surface", $"(upper(code) IN ({surfaceTopologyFeatures}))", de9im[j])) {
+                        if (lookup.Contains(f.objectid)) continue;
+
+                        var shape = (ArcGIS.Core.Geometry.Polygon)f.shape;
+
+                        var name = f.UID;   // Convert.ToString(f["UID"]);
+                        if (string.IsNullOrEmpty(name))
+                            name = string.Empty;
+
+                        shape = (Polygon)clipGeometry(shape);
+                        if (shape.IsEmpty) continue;
+
+                        var exteriorRing = shape.GetExteriorRing(0);
+                        var coordinates = exteriorRing.Parts[0].Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.StartPoint.X, segment.StartPoint.Y)).ToArray();
+
+                        var ex = factory.CreateLinearRing([.. coordinates, coordinates[0]]);
+
+                        var reduced = matrix.Reducer.Reduce(ex);
+                        if (!(reduced is LinearRing linear)) continue;
+
+                        ex = (LinearRing)reduced;
+
+                        if (shape.PartCount > 1) {
+                            var interiorRings = new List<LinearRing>();
+
+                            foreach (var interiorRing in shape.Parts.Skip(1)) {
+                                coordinates = interiorRing.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.StartPoint.X, segment.StartPoint.Y)).ToArray();
+
+                                var linestring = factory.CreateLinearRing([.. coordinates, coordinates[0]]);
+                                linestring = (LinearRing)matrix.Reducer.Reduce(linestring);
+                                linestring = (LinearRing)TopologyPreservingSimplifier.Simplify(linestring, 0.0);
+
+                                if (!linestring.IsSelfIntersections())
+                                    interiorRings.Add(linestring);
+                                else {
+                                    foreach (var l in SplitAtSelfIntersections(linestring)) {
+                                        if (l.IsRing)
+                                            interiorRings.Add(l.Factory.CreateLinearRing(l.Coordinates));
+                                        else //if(l.Coordinates.Length>3)
+                                            System.Diagnostics.Debugger.Break();
+                                    }
+                                }
+                            }
+
+                            polygons.Add(new S100FC.Topology.Polygon(f.GetObjectID(), name, Convert.ToString(f["code"])!, ex, interiorRings.ToArray()));
+                        }
+                        else {
+                            polygons.Add(new S100FC.Topology.Polygon(f.GetObjectID(), name, Convert.ToString(f["code"])!, ex, []));
+                        }
+                    }
+                }
+
+                var curves = new List<S100FC.Topology.Polyline>();
+
+                for (int j = 0; j < de9im.Length; j++) {
+                    var lookup = curves.ToLookup(e => e.ObjectId, e => e);
+
+                    foreach (var f in features.Invoke("curve", $"(upper(code) IN ({curveTopologyFeatures}))", de9im[j])) {
+                        if (lookup.Contains(f.GetObjectID())) continue;
+
+                        var shape = (Polyline)f.GetShape();
+
+                        shape = (Polyline)clipGeometry(shape);
+                        if (shape.IsEmpty) continue;
+
+                        var name = Convert.ToString(f["UID"]);
+                        if (string.IsNullOrEmpty(name))
+                            name = string.Empty;
+
+                        LineString[] parts = [];
+                        foreach (var part in shape.Parts) {
+                            var p = PolylineBuilderEx.CreatePolyline(part);
+
+                            var coordinates = p.Points.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.X, segment.Y)).ToArray();
+
+                            var linestring = factory.CreateLineString([.. coordinates]);
+                            linestring = (LineString)matrix.Reducer.Reduce(linestring);
+                            linestring = (LineString)TopologyPreservingSimplifier.Simplify(linestring, 0.0);
+
+                            if (!linestring.IsSelfIntersections())
+                                parts = [.. parts, linestring];
+                            else {
+                                foreach (var l in SplitAtSelfIntersections(linestring))
+                                    parts = [.. parts, l];
+                            }
+                        }
+                        if (parts.Length == 1) {
+                            curves.Add(new S100FC.Topology.Polyline(f.GetObjectID(), name, Convert.ToString(f["code"])!, parts[0], name));
+                        }
+                        else {
+                            for (int i = 0; i < parts.Length; i++) {
+                                curves.Add(new S100FC.Topology.Polyline(f.GetObjectID(), $"{name}:{i}", Convert.ToString(f["code"])!, parts[i], name));
+                                mapper.Add($"{name}:{i}", name);
+                            }
+                        }
+                    }
+                }
+
+                builder = matrix.AddTopologyFeatures(polygons, curves);
+            }
+
+            //  Navigational features
+            {
+                var polygons = new List<S100FC.Topology.Polygon>();
+
+                string[] de9im = [Matrix.DE9IM_Contains, Matrix.DE9IM_Crosses];
+                for (int j = 0; j < de9im.Length; j++) {
+                    var lookup = polygons.ToLookup(e => e.ObjectId, e => e);
+
+                    foreach (var f in features.Invoke("surface", $"(upper(code) NOT IN ({surfaceTopologyFeatures}))", de9im[j])) {
+                        if (lookup.Contains(f.GetObjectID())) continue;
+
+#if SKIN_OF_THE_EARTH_ONLY
+                            if (!testFeatures.Contains(Convert.ToString(f["code"]))) continue;
+#endif
+                        var shape = (ArcGIS.Core.Geometry.Polygon)f.GetShape();
+
+                        var name = Convert.ToString(f["UID"]);
+                        if (string.IsNullOrEmpty(name))
+                            name = string.Empty;
+
+                        shape = (Polygon)clipGeometry(shape);
+                        if (shape.IsEmpty) continue;
+
+                        var exteriorRing = shape.GetExteriorRing(0);
+                        var coordinates = exteriorRing.Parts[0].Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.StartPoint.X, segment.StartPoint.Y)).ToArray();
+
+                        var ex = factory.CreateLinearRing([.. coordinates, coordinates[0]]);
+
+                        var reduced = matrix.Reducer.Reduce(ex);
+                        if (!(reduced is LinearRing linear)) continue;
+
+                        ex = (LinearRing)reduced;
+
+                        if (shape.PartCount > 1) {
+                            var interiorRings = new List<LineString>();
+
+                            var index = 1;
+                            foreach (var interiorRing in shape.Parts.Skip(1)) {
+                                coordinates = CoordinateArrays.RemoveRepeatedPoints(interiorRing.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.StartPoint.X, segment.StartPoint.Y)).ToArray());
+
+                                var linestring = factory.CreateLinearRing([.. coordinates, coordinates[0]]);
+                                linestring = (LinearRing)matrix.Reducer.Reduce(linestring);
+                                linestring = (LinearRing)TopologyPreservingSimplifier.Simplify(linestring, 0.0);
+
+                                if (!linestring.IsSelfIntersections())
+                                    interiorRings.Add(linestring);
+                                else {
+                                    foreach (var l in SplitAtSelfIntersections(linestring)) {
+                                        if (l.IsRing)
+                                            interiorRings.Add(l.Factory.CreateLinearRing(l.Coordinates));
+                                        else if (l.Count <= 3) {
+                                            ;
+                                        }
+                                        else {//if(l.Coordinates.Length>3)
+                                            System.Diagnostics.Debugger.Break();
+                                            interceptor?.Invoke(100, [.. SplitAtSelfIntersections(linestring).Select(e => (e, name))], true);
+                                        }
+                                    }
+                                }
+
+                                //if (linestring.IsSelfIntersections()) {
+                                //        interceptor?.Invoke(100, [((linestring, $"{name}::i{index}"))]);
+
+                                //    System.Diagnostics.Debugger.Break();
+                                //    //linestring = factory.CreateLinearRing(CoordinateArrays.RemoveRepeatedPoints(linestring.Coordinates));
+                                //    var split = SplitAtSelfIntersections(linestring);
+
+                                //    interceptor?.Invoke(100, [.. split.Select(l => (l, $"{name}::i{index}"))]);
+                                //}
+
+                                index += 1;
+                            }
+                            polygons.Add(new S100FC.Topology.Polygon(f.GetObjectID(), name, Convert.ToString(f["code"])!, ex, interiorRings.ToArray()));
+                        }
+                        else {
+                            polygons.Add(new S100FC.Topology.Polygon(f.GetObjectID(), name, Convert.ToString(f["code"])!, ex, []));
+                        }
+                    }
+                }
+
+                var curves = new List<S100FC.Topology.Polyline>();
+                var singletons = new List<S100FC.Topology.Polyline>();
+
+                var singletonsFeatures = "''";// "'ROAD','RAILWAY'";  //'NAVIGATIONLINE','RECOMMENDEDTRACK'
+
+                for (int j = 0; j < de9im.Length; j++) {
+                    var lookup = curves.ToLookup(e => e.ObjectId, e => e);
+
+                    foreach (var f in features.Invoke("curve", $"(upper(code) NOT IN ({curveTopologyFeatures})) AND (upper(code) NOT IN ({singletonsFeatures}))", de9im[j])) {
+
+                        if (lookup.Contains(f.GetObjectID())) continue;
+
+#if SKIN_OF_THE_EARTH_ONLY
+                            continue;
+#endif
+
+                        var shape = (Polyline)f.GetShape();
+
+                        shape = (Polyline)clipGeometry(shape);
+                        if (shape.IsEmpty) continue;
+
+                        var name = Convert.ToString(f["UID"]);
+                        if (string.IsNullOrEmpty(name))
+                            name = string.Empty;
+
+                        LineString[] parts = [];
+                        foreach (var part in shape.Parts) {
+                            var p = PolylineBuilderEx.CreatePolyline(part);
+
+                            var coordinates = p.Points.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.X, segment.Y)).ToArray();
+
+                            var linestring = factory.CreateLineString([.. coordinates]);
+                            linestring = (LineString)matrix.Reducer.Reduce(linestring);
+                            linestring = (LineString)TopologyPreservingSimplifier.Simplify(linestring, 0.0);
+
+                            if (!linestring.IsSelfIntersections())
+                                parts = [.. parts, linestring];
+                            else {
+                                foreach (var l in SplitAtSelfIntersections(linestring))
+                                    parts = [.. parts, l];
+                            }
+                        }
+                        if (parts.Length == 1) {
+                            curves.Add(new S100FC.Topology.Polyline(f.GetObjectID(), name, Convert.ToString(f["code"])!, parts[0], name));
+                        }
+                        else {
+                            for (int i = 0; i < parts.Length; i++) {
+                                curves.Add(new S100FC.Topology.Polyline(f.GetObjectID(), $"{name}:{i}", Convert.ToString(f["code"])!, parts[i], name));
+                                mapper.Add($"{name}:{i}", name);
+                            }
+                        }
+                    }
+                }
+#if Singletons
+                    foreach (var filter in filters) {
+                        filter.WhereClause = (!string.IsNullOrEmpty(whereClause) ? $"{whereClause} AND " : "") + $"(upper(code) IN ({singletonsFeatures}))";
+
+                        using var cursor = curve.Search(filter);
+
+                        var lookup = singletons.ToLookup(e => e.ObjectId, e => e);
+
+                        while (cursor.MoveNext()) {
+                            var f = (Feature)cursor.Current;
+
+                            if (lookup.Contains(f.GetObjectID())) continue;
+
+                            //if (f.GetObjectID() == 44) System.Diagnostics.Debugger.Break();
+
+                            var shape = (Polyline)f.GetShape();
+
+                            shape = (Polyline)clipGeometry(shape);
+
+                            var name = Convert.ToString(f["UID"]);
+                            if (string.IsNullOrEmpty(name))
+                                name = string.Empty;
+
+                            //var coordinates = shape.Points.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.X, segment.Y)).ToArray();
+
+                            //var linestring = factory.CreateLineString([.. coordinates]);
+                            //linestring = linestring.RemoveRepeatedVertices();
+
+                            //singletons.Add(new S100FC.Topology.Polyline(f.GetObjectID(), name, Convert.ToString(f["code"])!, linestring));
+                            for (int i = 0; i < shape.PartCount; i++) {
+                                var p = PolylineBuilderEx.CreatePolyline(shape.Parts[i]);
+
+                                var coordinates = p.Points.Select(segment => new NetTopologySuite.Geometries.Coordinate(segment.X, segment.Y)).ToArray();
+
+                                var linestring = factory.CreateLineString([.. coordinates]);
+                                linestring = linestring.RemoveRepeatedVertices();
+
+                                singletons.Add(new S100FC.Topology.Polyline(f.GetObjectID(), $"{name}:p{i}", Convert.ToString(f["code"])!, linestring, name));                                
+                            }
+                        }
+                    }
+#endif
+
+
+                builder = matrix.AddTopologyFeatures(polygons, curves).AddSingletonFeatures(singletons);
+            }
+
+
+            var result = builder.BuildTopology();
+
+            //interceptor?.Invoke(6001, result.Curves.Select(e => (e.LineString, $"{e.Id}")).ToArray());
+
+            return (result, mapper);
+        }
+
+#if legacy
         public static (S100FC.Topology.IMatrix matrix, IDictionary<string, string> mapper) BuildTopology(this Geodatabase geodatabase, QueryFilter? queryFilter = default, Action<int, ICollection<(LineString lineString, string message)>, bool>? interceptor = default, ILoggerFactory? loggerFactory = default) {
             var syntax = geodatabase.GetSQLSyntax();
 
@@ -174,7 +489,7 @@ namespace ArcGIS.Core.Geometry
 
 
             var logger = loggerFactory?.CreateLogger<Reloaded>();
-            //var matrix = S100FC.Topology.Matrix.CreateMatrix(interceptor);
+
             var matrix = S100FC.Topology.Reloaded.CreateMatrix(interceptor, logger);
 
             S100FC.Topology.ITopologyBuilder? builder = default;
@@ -604,6 +919,7 @@ namespace ArcGIS.Core.Geometry
 
             return (result, mapper);
         }
+#endif
 
         private const double _snapTolerance = 0.000000001;
 
